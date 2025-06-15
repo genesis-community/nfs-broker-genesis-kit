c3d0597e25cd19788b55fbe0d35b0f3bc70b1a78
Wayne E. Seguin <wayneeseguin@gmail.com>
2025-06-15 17:32:23 -0400

    Standardize Perl hook modules for Genesis compatibility
      - Remove shebangs and version declarations from all hook modules
      - Update vim modelines to follow Genesis coding standards
      - Fix parent class inheritance for typed hooks (Check, Info, New, etc.)
      - Standardize BEGIN block format for development library paths
      - Remove redundant return statements after $self->done()
      - Clean up package declarations and imports

--- hooks/addon-register-broker~rb.pm
+++ hooks/addon-register-broker~rb.pm
@@ -1,15 +1,14 @@
-#!/usr/bin/env perl
-# vim: set ts=2 sw=2 sts=2 foldmethod=marker
-package Genesis::Hook::Addon::NFS::RegisterBroker v1.0.0;
+# vim: set ts=2 sw=2 sts=2 noet fdm=marker foldlevel=1:
+package Genesis::Hook::Addon::NFS::RegisterBroker;
 
-use strict;
-use warnings;
-use v5.20; # Genesis min perl version is 5.20
+use v5.20;
+use warnings; # Genesis min perl version is 5.20
 use Genesis qw/bail info run/;
+# Only needed for development
+BEGIN {push @INC, $ENV{GENESIS_LIB} ? $ENV{GENESIS_LIB} : $ENV{HOME}.'./.genesis/lib'}
+
 use parent qw(Genesis::Hook::Addon);
 use Genesis::UI qw(prompt_for_boolean prompt_for);
-use lib $ENV{GENESIS_LIB} // "$ENV{HOME}/.genesis/lib";
-
 sub init {
 	my $class = shift;
 	my $obj = $class->SUPER::init(@_);
--- hooks/features.pm
+++ hooks/features.pm
@@ -1,10 +1,8 @@
-#!/usr/bin/env perl
-# vim: set ts=2 sw=2 sts=2 foldmethod=marker
-package Genesis::Hook::Features::NFS v1.0.0;
+# vim: set ts=2 sw=2 sts=2 noet fdm=marker foldlevel=1:
+package Genesis::Hook::Features::NFS;
 
-use strict;
-use warnings;
-use v5.20; # Genesis min perl version is 5.20
+use v5.20;
+use warnings; # Genesis min perl version is 5.20
 
 # Only needed for development
 BEGIN {push @INC, $ENV{GENESIS_LIB} ? $ENV{GENESIS_LIB} : $ENV{HOME}.'/.genesis/lib'}
@@ -39,6 +37,9 @@ sub perform {
 	}
 
 	return $self->build_features_list();
+
+	return 1;
+
 }
 
 1;
